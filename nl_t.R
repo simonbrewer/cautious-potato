679368c63eb4d5c60b5d09ba39c003f14dd08568
Simon Brewer <brewersi@gmail.com>
2026-01-24 16:04:53 -0700

    Non-linear treatment

---
+++ nl_t.R
@@ -0,0 +1,76 @@
+## GRF with treatment in features (second stage)
+
+library(grf)
+library(hstats)
+
+# set.seed(42)
+n <- 2000 ## n samples
+p <- 4 ## p features
+
+## Make random features
+X <- matrix(rnorm(n * p), n, p)
+# X <- matrix(0, n, p)
+
+## Needed for predictions
+X_test0 <- matrix(0, 101, p)
+X_test1 <- cbind(X_test0, seq(-3, 3, length.out = 101))
+# X_test[, 1] <- seq(-2, 2, length.out = 101)
+
+## Make random treatment (cont)
+W <- rnorm(n, mean = 0)
+
+## Make outcome: nonlinear treatment effect only
+w_eff <- 2
+Y <- pmax(W, 0) * w_eff
+# Y <- W * w_eff
+
+## Noise on Y
+Y_e <- rnorm(n, sd = 0.25)
+Y <- Y + Y_e
+
+## Plot treatment 'effect'
+plot(W, Y)
+
+## ---------------------------------------------
+## Fit causal forest (usual)
+## ---------------------------------------------
+## In stages
+colnames(X) <- c("X0", "X1", "X2", "X3")
+
+f_W <- regression_forest(X, W)
+W_hat <- predict(f_W)$predictions
+
+f_Y <- regression_forest(X, Y)
+Y_hat <- predict(f_Y)$predictions
+
+f_tau0 <- causal_forest(X, Y, W,
+                            W.hat = W_hat, Y.hat = Y_hat)
+f_tau0
+average_treatment_effect(f_tau0)
+
+## importance
+imp <- sort(setNames(variable_importance(f_tau0), colnames(X)))
+par(mai = c(0.7, 2, 0.2, 0.2))
+barplot(imp, horiz = TRUE, las = 1, col = "orange")
+
+## pdp
+plot(partial_dep(f_tau0, "X3", X = X))
+
+## ---------------------------------------------
+## Fit causal forest (T in X)
+## ---------------------------------------------
+X_W = cbind(X, W)
+colnames(X_W) <- c("X0", "X1", "X2", "X3", "T")
+
+f_tau1 <- causal_forest(X_W, Y, W,
+                        W.hat = W_hat, Y.hat = Y_hat)
+f_tau1
+average_treatment_effect(f_tau1)
+
+## importance
+imp <- sort(setNames(variable_importance(f_tau1), colnames(X_W)))
+par(mai = c(0.7, 2, 0.2, 0.2))
+barplot(imp, horiz = TRUE, las = 1, col = "orange")
+
+## pdp
+plot(partial_dep(f_tau1, "T", X = X_W))
